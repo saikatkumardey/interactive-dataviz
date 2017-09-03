898db3698170009560f89460396e0d89e295dcb3
Saikat Dey <saikat.d@endurance.com>
2017-09-03 17:38:54 +0530

    experimental port

--- app.py
+++ app.py
@@ -60,9 +60,9 @@ def update_graph(species_types,column):
 
 if __name__ == '__main__':
 
-    if len(sys.argv) > 1:
-        port = int(sys.argv[1])
-    else:
-        port = 80
-
+    # if len(sys.argv) > 1:
+    #     port = int(sys.argv[1])
+    # else:
+    #     port = 80
+    port=80
     app.run_server(host="0.0.0.0",port=port,debug=True)
